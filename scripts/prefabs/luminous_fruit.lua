7bfc548b61842bbe6f0057b42b3da944f94e155d
XenoMind <darkratchet54@yahoo.com>
2023-10-19 17:27:46 -0400

    Added Fruit for trees
    Both raw and uncooked. They don't do anything. Still haven't finished sapling.
    
    Must do another pass on all art tonight.
    
     Also I hate trees D:<

---
+++ scripts/prefabs/luminous_fruit.lua
@@ -0,0 +1,167 @@
+require "prefabutil"
+
+local assets =
+{
+    Asset("ANIM", "anim/luminous_fruit.zip"),
+}
+
+local prefabs =
+{
+---sapling not done
+    --"luminous_fruit_sapling",
+    "luminous_fruit_cooked",
+    "spoiled_food",
+   
+}
+
+---Not finished
+--[[local function plant(pt, growtime)
+    local sapling = SpawnPrefab("luminous_fruit_sapling")
+    sapling:StartGrowing()
+    sapling.Transform:SetPosition(pt:Get())
+    sapling.SoundEmitter:PlaySound("dontstarve/wilson/plant_tree")
+end
+
+
+local function ondeploy(inst, pt)
+    inst = inst.components.stackable:Get()
+    inst:Remove()
+
+    local timeToGrow = GetRandomWithVariance(TUNING.ACORN_GROWTIME.base, TUNING.ACORN_GROWTIME.random)
+    plant(pt, timeToGrow)
+
+   
+end
+
+local function OnLoad(inst, data)
+    if data and data.growtime then
+        plant(inst, data.growtime)
+    end
+end]]
+
+local function fn()
+    local inst = CreateEntity()
+
+    inst.entity:AddTransform()
+    inst.entity:AddAnimState()
+    inst.entity:AddSoundEmitter()
+    inst.entity:AddNetwork()
+
+    MakeInventoryPhysics(inst)
+
+    inst.AnimState:SetBank("luminous_fruit")
+    inst.AnimState:SetBuild("luminous_fruit")
+    inst.AnimState:PlayAnimation("idle")
+
+    inst:AddTag("deployedplant")
+    inst:AddTag("icebox_valid")
+    inst:AddTag("cattoy")
+    inst:AddTag("show_spoilage")
+    inst:AddTag("treeseed")
+
+ 
+    inst:AddTag("cookable")
+
+    MakeInventoryFloatable(inst, "small", 0.15)
+
+    inst.entity:SetPristine()
+
+    if not TheWorld.ismastersim then
+        return inst
+    end
+
+    inst:AddComponent("cookable")
+    inst.components.cookable.product = "luminous_fruit_cooked"
+
+    inst:AddComponent("tradable")
+
+    inst:AddComponent("perishable")
+    inst.components.perishable:SetPerishTime(TUNING.PERISH_PRESERVED)
+    inst.components.perishable:StartPerishing()
+    inst.components.perishable.onperishreplacement = "spoiled_food"
+
+    inst:AddComponent("edible")
+    inst.components.edible.hungervalue = TUNING.CALORIES_TINY
+    inst.components.edible.healthvalue = TUNING.HEALING_TINY
+    inst.components.edible.foodtype = FOODTYPE.RAW
+
+    inst:AddComponent("stackable")
+    inst.components.stackable.maxsize = TUNING.STACK_SIZE_SMALLITEM
+
+    inst:AddComponent("inspectable")
+
+    MakeSmallBurnable(inst, TUNING.SMALL_BURNTIME)
+    MakeSmallPropagator(inst)
+
+    inst:AddComponent("inventoryitem")
+
+    --[[inst:AddComponent("deployable")
+    inst.components.deployable:SetDeployMode(DEPLOYMODE.PLANT)
+    inst.components.deployable.ondeploy = ondeploy]]
+
+    inst:AddComponent("forcecompostable")
+    inst.components.forcecompostable.brown = true
+
+    MakeHauntableLaunchAndIgnite(inst)
+
+    --inst.OnLoad = OnLoad
+
+    return inst
+end
+
+local function cooked()
+    local inst = CreateEntity()
+
+    inst.entity:AddTransform()
+    inst.entity:AddAnimState()
+    inst.entity:AddSoundEmitter()
+    inst.entity:AddNetwork()
+
+    MakeInventoryPhysics(inst)
+
+    inst.AnimState:SetBank("luminous_fruit")
+    inst.AnimState:SetBuild("luminous_fruit")
+    inst.AnimState:PlayAnimation("cooked")
+
+    MakeInventoryFloatable(inst, "small", 0.1)
+
+    inst.entity:SetPristine()
+
+    if not TheWorld.ismastersim then
+        return inst
+    end
+
+    inst:AddComponent("edible")
+    inst.components.edible.hungervalue = TUNING.CALORIES_TINY
+    inst.components.edible.healthvalue = TUNING.HEALING_TINY
+    inst.components.edible.foodtype = FOODTYPE.BERRY
+	
+
+    inst:AddComponent("tradable")
+
+    inst:AddComponent("perishable")
+    inst.components.perishable:SetPerishTime(TUNING.PERISH_FAST)
+    inst.components.perishable:StartPerishing()
+    inst.components.perishable.onperishreplacement = "spoiled_food"
+
+    inst:AddComponent("stackable")
+    inst.components.stackable.maxsize = TUNING.STACK_SIZE_MEDITEM
+
+    inst:AddComponent("inspectable")
+
+    MakeSmallBurnable(inst, TUNING.SMALL_BURNTIME)
+    MakeSmallPropagator(inst)
+
+    inst:AddComponent("inventoryitem")
+
+    inst:AddComponent("forcecompostable")
+    inst.components.forcecompostable.brown = true
+
+    MakeHauntableLaunch(inst)
+
+    return inst
+end
+
+return Prefab("luminous_fruit", fn, assets, prefabs),
+       Prefab("luminous_fruit_cooked", cooked, assets)
+       --MakePlacer("luminous_fruit_placer", "luminous_fruit", "luminous_fruit", "idle_planted")
