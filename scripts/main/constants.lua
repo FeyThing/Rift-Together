5782c169e064eb47345356ba9d063457b64ca429
XenoMind <darkratchet54@yahoo.com>
2024-01-29 20:59:45 -0500

    Updated constants

--- scripts/main/constants.lua
+++ scripts/main/constants.lua
@@ -7,7 +7,7 @@ MOD =
 		CHERRY_ENABLED = GLOBAL.KnownModIndex:IsModEnabled("workshop-1289779251"),
 		UNCOMP_ENABLED = GLOBAL.KnownModIndex:IsModEnabled("workshop-2039181790"),
 		DEHYDRATED_ENABLED = GLOBAL.KnownModIndex:IsModEnabled("workshop-3004639365"),
-		SHOES_ENABLED = GLOBAL.KnownModIndex:GetModActualName("Shoetime"),  
+		SHOES_ENABLED = GLOBAL.KnownModIndex:IsModEnabled("workshop-3150203711"),  
 	}
 
 --- Materials
